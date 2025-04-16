433f3c23c1b621c08b6b3c8e6faa7f15dcda6016
Cássio M. Silva (Dev) <dev.cassiomsilva@gmail.com>
2025-04-16 16:24:32 -0300

    refactor: start createDefaultData job

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ const config = {
   },
   "serverStartComplete": () => {
     console.log('Parse server started');
-    // Parse.Cloud.startJob("createDefaultData", {"data": defaultData});
+    Parse.Cloud.startJob("createDefaultData", {"data": defaultData});
   }
 };
 
