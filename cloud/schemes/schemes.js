0e23aa90083b60e998262b823608c6fd77f6d22d
Cássio M. Silva (Dev) <dev.cassiomsilva@gmail.com>
2025-04-16 16:24:11 -0300

    refactor: use new schemes

--- cloud/schemes/schemes.js
+++ cloud/schemes/schemes.js
@@ -1,3 +1,4 @@
+const { RoleScheme, RoleDefaultData } = require('./role');
 const { InstallationScheme, InstallationDefaultData } = require('./installation');
 const { UserScheme, UserDefaultData } = require('./user');
 const { UserDeletedScheme, UserDeletedDefaultData } = require('./user_deleted');
@@ -7,30 +8,57 @@ const { UserTermsScheme, UserTermDefaultData } = require('./user_term');
 const { PushNotificationScheme, PushNotificationDefaultData } = require('./push_notification');
 const { NotificationScheme, NotificationDefaultData } = require('./notification');
 
+const { CompanyScheme, CompanyDefaultData } = require('./company');
+
+const { ProductStatusScheme, ProductStatusDefaultData } = require('./product_status');
+const { ProductCategoryScheme, ProductCategoryDefaultData } = require('./product_category');
+const { ProductScheme, ProductDefaultData } = require('./product');
+
+const { ProductOrderScheme, ProductOrderDefaultData } = require('./product_order');
+
 const { PaymentTypeScheme, PaymentTypeDefaultData } = require('./payment_type');
+const { OrderStatusScheme, OrderStatusDefaultData } = require('./order_status');
+const { OrderScheme, OrderDefaultData } = require('./order');
 
 const schemes = [
-  //Installation,
-  //User,
-  //UserDeleted,
-  //VersionApp,
-  // Terms,
-  // UserTerms,
-  //PushNotification,
-  //Notification,
+  RoleScheme, // Default
+  InstallationScheme, // Default
+  CompanyScheme,
+  UserScheme, // Default
+  UserDeletedScheme, // Default
+  VersionAppScheme, // Default
+  // TermsScheme,
+  // UserTermsScheme,
+  PushNotificationScheme, // Default
+  NotificationScheme, // Default
+  //
+  ProductStatusScheme,
+  ProductCategoryScheme,
+  ProductScheme,
+  ProductOrderScheme,
   PaymentTypeScheme,
+  OrderStatusScheme,
+  OrderScheme,
 ];
 
 const defaultData = [
-  InstallationDefaultData,
-  UserDefaultData,
-  UserDeletedDefaultData,
-  VersionAppDefaultData,
-  TermDefaultData,
-  UserTermDefaultData,
-  PushNotificationDefaultData,
-  NotificationDefaultData,
+  RoleDefaultData, // Default
+  InstallationDefaultData, // Default
+  CompanyDefaultData,
+  UserDefaultData, // Default
+  UserDeletedDefaultData, // Default
+  VersionAppDefaultData, // Default
+  TermDefaultData, // Default
+  UserTermDefaultData, // Default
+  PushNotificationDefaultData, // Default
+  NotificationDefaultData, // Default
+  ProductStatusDefaultData,
+  ProductCategoryDefaultData,
+  ProductDefaultData,
+  ProductOrderDefaultData,
   PaymentTypeDefaultData,
+  OrderStatusDefaultData,
+  OrderDefaultData,
 ];
 
 module.exports = { schemes, defaultData };
