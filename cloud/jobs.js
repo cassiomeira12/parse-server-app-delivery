e6b0b931a6c6f42dc5ff3f2d5358b4d327a4c0a0
Cássio M. Silva (Dev) <dev.cassiomsilva@gmail.com>
2025-04-16 16:25:08 -0300

    refactor: add new jobs

--- cloud/jobs.js
+++ cloud/jobs.js
@@ -1,27 +1,98 @@
-// Parse.Cloud.job("checkWhatsAppServers2", async (request) => {
-//   let date = new Date();
-//   let timeNow = date.getTime();
-//   let intervalOfTime = 3 * 60 * 1000;  // the time set is 3 minutes in milliseconds
-//   let timeThen = timeNow - intervalOfTime;
+var schedule = require('node-schedule');
 
-//   // Limit date
-//   let queryDate = new Date();
-//   queryDate.setTime(timeThen);
+// *    *    *    *    *    *
+// second (0 - 59, OPTIONAL)
+// minute (0 - 59)
+// hour (0 - 23)
+// day of month (1 - 31)
+// month (1 - 12)
+// day of week (0 - 7) (0 or 7 is Sun)
 
-//   // The query object
-//   let querySession = new Parse.Query(Parse.Session);
+// '*/5 * * * *' every 5 min
 
-//   querySession.lessThanOrEqualTo("expiresAt", date);
+// Schedule 00:00h envery day
+schedule.scheduleJob('*/1 * * * *', function () {
+  Parse.Cloud.startJob("clearOldSessions");
+});
 
-//   const results = await querySession.find({ useMasterKey: true });
+Parse.Cloud.job("clearOldSessions", async (request) => {
+  let date = new Date();
+  let timeNow = date.getTime();
+  let intervalOfTime = 3 * 60 * 1000;  // the time set is 3 minutes in milliseconds
+  let timeThen = timeNow - intervalOfTime;
 
-//   results.forEach(object => {
-//     object.destroy({ useMasterKey: true }).then(destroyed => {
-//       console.log("Successfully destroyed object" + JSON.stringify(destroyed));
-//     }).catch(error => {
-//       console.log("Error: " + error.code + " - " + error.message);
-//     })
-//   });
+  // Limit date
+  let queryDate = new Date();
+  queryDate.setTime(timeThen);
 
-//   return ("Successfully retrieved " + results.length + " invalid logins.");
-// });
+  // The query object
+  let querySession = new Parse.Query(Parse.Session);
+
+  querySession.lessThanOrEqualTo("expiresAt", date);
+
+  const results = await querySession.find({ useMasterKey: true });
+
+  results.forEach(object => {
+    object.destroy({ useMasterKey: true }).then(destroyed => {
+      console.log("Successfully destroyed object" + JSON.stringify(destroyed));
+    }).catch(error => {
+      console.log("Error: " + error.code + " - " + error.message);
+    })
+  });
+
+  return ("Successfully retrieved " + results.length + " invalid logins.");
+});
+
+Parse.Cloud.job("createDefaultData", async (request) => {
+  const { params } = request;
+
+  const defaultData = params.data;
+
+  await Promise.all(defaultData.map(async (data) => {
+    const className = data.class;
+    const classData = data.items;
+    console.log(className);
+    
+    await Promise.all(classData.map(async (item) => {
+      const query = new Parse.Query(className);
+      const object = new Parse.Object(className);
+
+      for (const key in item) {
+        if (key !== 'password') {
+          query.equalTo(key, item[key]);
+        }
+        object.set(key, item[key]);
+      }
+
+      const result = await query.find({ useMasterKey: true });
+
+      if (result.length == 0) {
+        try {
+          var acl = new Parse.ACL();
+          acl.setPublicReadAccess(true);
+          acl.setPublicWriteAccess(false);
+          if (className !== "_Role") {
+            acl.setRoleReadAccess("Admin", true);
+            acl.setRoleWriteAccess("Admin", true);
+          }
+          object.setACL(acl);
+
+          const objectResult = await object.save(null, { useMasterKey: true });
+          console.log(objectResult.toJSON());
+
+          if (className === "_User") {
+            const queryRole = new Parse.Query("_Role");
+            queryRole.equalTo('name', 'Admin');
+            const adminRole = await queryRole.first({ useMasterKey: true });
+            const users = adminRole.relation('users');
+            users.add(objectResult);
+            await adminRole.save(null, { useMasterKey: true });
+          }
+
+        } catch (exception) {
+          console.log(exception);
+        }
+      }
+    }));
+  }));
+});
